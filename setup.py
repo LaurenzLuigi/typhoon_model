944974a798d56b75bb9c68193cbef100b4c861da
LaurenzLuigi <laurenz.cruz@gmail.com>
2020-09-02 02:17:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ Created on Thu Aug 27 15:51:01 2020
 from setuptools import setup
 
 setup(
-      name="typhoonify",
+      name="typhoon_model",
       version="0.1",
       description="various utils for generating typhoon wind field",
-      packages=["typhoonify"],
-      install_requires=["scipy"]
+      packages=["typhoon_model"],
+      install_requires=["scipy, numpy, netcdf4"]
       )
